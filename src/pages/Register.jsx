ac6fe63b1fff1b38f6a39ce26571fcaeaace51be
anwarkvzk <anvarvzk521@gmail.com>
2023-09-30 08:39:06 +0530

    Frontend  backend connection proxy added

---
+++ src/pages/Register.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Form, Input } from "antd";
+import "../styles/Registerstyles.css";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  //form handler
+  const onFinishHandler = (values) => {
+    console.log(values);
+  };
+  return (
+    <>
+      <div className="form-container">
+        <Form
+          layout="vertical"
+          onFinish={onFinishHandler}
+          className="register-form"
+        >
+          <h3 className="text-center">Register Form</h3>
+          <Form.Item label="Name" name="name">
+            <Input type="text" required />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+          <Link to="/login" className="m-2">
+            Already User Login here
+          </Link>
+          <button className="btn btn-primary" type="submit">
+            Register
+          </button>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
