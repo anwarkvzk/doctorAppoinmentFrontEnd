cb9bcb24bfaf25f3a477205b0dd3c72e053a58bc
anwarkvzk <anvarvzk521@gmail.com>
2023-09-30 17:32:46 +0530

    axios login and jwt token added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { Form, Input } from "antd";
+import { Form, Input, message } from "antd";
 import "../styles/Registerstyles.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const navigate = useNavigate();
   //form handler
-  const onFinishHandler = (values) => {
-    console.log(values);
+  const onFinishHandler = async (values) => {
+    try {
+      const res = await axios.post("/api/v1/user/login", values);
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login Succesfully");
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      message.error("Something Went Wrong");
+    }
   };
   return (
     <>
