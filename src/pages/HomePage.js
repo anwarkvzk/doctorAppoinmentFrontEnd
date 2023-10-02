7cd5532a9b7f97e42a9a9acd04083ecc9e8b5794
anwarkvzk <anvarvzk521@gmail.com>
2023-10-02 23:45:09 +0530

    Home page data fetching with axios and auth controlling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from 'axios'
 
 function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+  //login user Data
+  const getUserData = async () => {
+    try {
+      const res = await axios.post('/api/v1/user/getUserData',{},{
+        headers:{
+          Authorization: "Bearer " + localStorage.getItem('token')
+        }
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+  return <div>HomePage</div>;
 }
 
-export default HomePage
+export default HomePage;
