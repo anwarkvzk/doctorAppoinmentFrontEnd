eb6de51d0deeb4deaa35bf6dcfb4bb9d68a8f5ef
anwarkvzk <anvarvzk521@gmail.com>
2023-10-08 12:43:07 +0530

    Sidebar Created and designed

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "../styles/LayoutStyles.css";
+import { SidebarMenu } from "../Data/data";
+import { Link, useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
+  const location = useLocation();
   return (
     <>
       <div className="main">
@@ -11,7 +14,19 @@ const Layout = ({ children }) => {
               <h6>Doc APP</h6>
               <hr />
             </div>
-            <div className="menu">Menu</div>
+            <div className="menu">
+              {SidebarMenu.map((menu) => {
+                const isActive = location.pathname === menu.path;
+                return (
+                  <>
+                    <div className={`menu-item ${isActive && "active"}`}>
+                      <i className={menu.icon}></i>
+                      <Link to={menu.path}>{menu.name}</Link>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
           </div>
           <div className="content">
             <div className="header">Header</div>
