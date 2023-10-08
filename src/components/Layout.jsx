2c4144362ece115ea87d4cd02fc648a87aff29ef
anwarkvzk <anvarvzk521@gmail.com>
2023-10-08 22:25:59 +0530

    doctor apply form notification icon created

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import "../styles/LayoutStyles.css";
 import { adminMenu, userMenu } from "../Data/data";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { message } from "antd";
+import { message, Badge } from "antd";
 
 const Layout = ({ children }) => {
   const { user } = useSelector((state) => state.user);
@@ -48,7 +48,9 @@ const Layout = ({ children }) => {
           <div className="content">
             <div className="header">
               <div className="header-content">
-                <i class="fa-solid fa-bell"></i>
+                <Badge count={user && user.notification.length}>
+                  <i class="fa-solid fa-bell"></i>
+                </Badge>
                 <Link to="/profile">{user?.name}</Link>
               </div>
             </div>
