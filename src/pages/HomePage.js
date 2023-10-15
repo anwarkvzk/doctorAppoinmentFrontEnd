90bf9f5af271c029af2bd720af859476e4baf3b8
anwarkvzk <anvarvzk521@gmail.com>
2023-10-15 11:45:22 +0530

    Home page doctors list and booking and appoinment

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,43 @@
-import React, { useEffect } from "react";
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Layout from "../components/Layout";
+import { Row } from "antd";
+import DoctorList from "../components/DoctorList";
 
 function HomePage() {
+  const [doctors, setDoctors] = useState([]);
   //login user Data
   const getUserData = async () => {
     try {
-      const res = await axios.post('/api/v1/user/getUserData',{},{
-        headers:{
-          Authorization: "Bearer " + localStorage.getItem('token')
+      const res = await axios.get(
+        "/api/v1/user/getAllDoctors",
+
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
         }
-      })
+      );
+      if (res.data.success) {
+        setDoctors(res.data.data);
+      }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
 
   useEffect(() => {
     getUserData();
   }, []);
-  return <Layout>Home Page</Layout>;
+  return (
+    <Layout>
+      <h1 className="text-center">
+        <Row>
+          {doctors && doctors.map((doctor) => <DoctorList doctor={doctor} />)}
+        </Row>
+      </h1>
+    </Layout>
+  );
 }
 
 export default HomePage;
