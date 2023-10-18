ae7692779062abae504d8eed78288b01c751e3bd
anwarkvzk <anvarvzk521@gmail.com>
2023-10-18 22:14:14 +0530

    booking Functionality added

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -2,15 +2,19 @@ import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { DatePicker, TimePicker } from "antd";
+import { DatePicker, TimePicker, message } from "antd";
 import moment from "moment";
+import { useDispatch, useSelector } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
 
 const BookingPage = () => {
+  const { user } = useSelector((state) => state.user);
   const [doctors, setDoctors] = useState([]);
   const [date, setDate] = useState();
-  const [timings, setTimings] = useState();
-  const [isAvailable, setIsAvailable] = useState();
+  const [time, setTime] = useState();
+  const [isAvailable, setIsAvailable] = useState(false);
   const params = useParams();
+  const dispatch = useDispatch();
   //login user Data
   const getUserData = async () => {
     try {
@@ -33,6 +37,69 @@ const BookingPage = () => {
     }
   };
 
+  // booking function
+  const handleBooking = async () => {
+    setIsAvailable(true);
+    if (!date && !time) {
+      return alert("Date & Time Required");
+    }
+    try {
+      dispatch(showLoading());
+      const res = await axios.post(
+        "/api/v1/user/book-appointment",
+        {
+          doctorId: params.doctorId,
+          userId: user._id,
+          doctorInfo: doctors,
+          date: date,
+          userInfo: user,
+          time: time,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (res.data.success) {
+        message.success(res.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      console.log(error);
+    }
+  };
+
+  const handleAvailability = async () => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post(
+        "/api/v1/user/booking-availability",
+        {
+          doctorId: params.doctorId,
+          date,
+          time,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (res.data.success) {
+        setIsAvailable(true);
+        message.success(res.data.message);
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getUserData();
     //eslint-disable-next-line
@@ -48,29 +115,36 @@ const BookingPage = () => {
             </h4>
             <h4>Fees : {doctors.feesPerConsaltation}</h4>
             {/* <h4>
-                Timings : {doctors.timings[0]} - {doctors.timings[1]}
-              </h4> */}
+              Timings : {doctors.timings[0]} - {doctors.timings[1]}
+            </h4> */}
             <div className="d-flex flex-column w-50">
               <DatePicker
                 className="m-2"
                 format="DD-MM-YYYY"
-                onChange={(value) =>
-                  setDate(moment(value).format("DD-MM-YYYY"))
-                }
+                onChange={(value) => {
+                  isAvailable(false);
+                  setDate(moment(value).format("DD-MM-YYYY"));
+                }}
               />
-              <TimePicker.RangePicker
+              <TimePicker
                 format="HH:mm"
                 className="m-2"
-                onChange={(values) =>
-                  setTimings([
-                    moment(values[0]).format("HH:mm"),
-                    moment(values[1]).format("HH:mm"),
-                  ])
-                }
+                onChange={(value) => {
+                  isAvailable(false);
+                  setTime(moment(value)).format("HH:mm");
+                }}
               />
-              <button className="btn btn-primary mt-2">
+              <button
+                className="btn btn-primary mt-2"
+                onClick={handleAvailability}
+              >
                 Check Availability
               </button>
+              {!isAvailable && (
+                <button className="btn btn-dark mt-2" onClick={handleBooking}>
+                  Book Now
+                </button>
+              )}
             </div>
           </div>
         )}
